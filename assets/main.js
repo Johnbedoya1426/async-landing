495abd6e9cf9cc60f791481b5e95b1b1e535b46c
Juan Lopez <jabedoya1426@gmail.com>
2022-09-29 16:08:06 -0500

    Updates

---
+++ assets/main.js
@@ -0,0 +1,45 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC7ZRAt7eWXsmanQ3x4EWmZw&part=snippet%2Cid&order=date&maxResults=15';
+
+const content = null || document.getElementById('content');
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5297ade834msh036360c9b61ba8ep1a1795jsn507492d493aa',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data
+};
+
+(async () => {
+    try {
+        const videos = await fetchData(API);
+        let view = `
+            ${videos.items.map(video => `
+                <div class="group relative">
+                <div
+                    class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                    <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                </div>
+                <div class="mt-4 flex justify-between">
+                    <h3 class="text-sm text-white">
+                    <span aria-hidden="true" class="absolute inset-0"></span>
+                    ${video.snippet.title}
+                    </h3>
+                </div>
+                </div>
+            `).slice(0,4).join('')}
+            `;
+            content.innerHTML = view;
+
+    } catch(error){
+        console.log(error);
+        }
+
+
+})();
+       
